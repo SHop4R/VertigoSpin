01ce7c5da6719e32c981c1953d3e1dc445a9cc66
Ege Akarsu <akarsu.ege@gmail.com>
2026-03-12 03:25:51 +0300

    Coin System Added

---
+++ Assets/Project/Scripts/Managers/CoinManager.cs
@@ -0,0 +1,62 @@
+using UnityEngine;
+using VertigoSpin.Project.Scripts.Utils;
+
+namespace VertigoSpin.Project.Scripts.Managers
+{
+    public sealed class CoinManager : MonoSingleton<CoinManager>
+    {
+        private const string CoinKey = "PlayerCoins";
+        private const int BaseReviveCost = 50;
+
+        public int Coins { get; private set; }
+        public int CurrentReviveCost { get; private set; } = BaseReviveCost;
+
+        public bool CanAffordRevive => Coins >= CurrentReviveCost;
+
+        private void Start()
+        {
+            Coins = PlayerPrefs.GetInt(CoinKey, 150);
+        }
+
+        private void OnEnable()
+        {
+            EventManager.GameEvents.OnGameOver += ResetReviveCost;
+            EventManager.GameEvents.OnGameRestart += ResetReviveCost;
+        }
+
+        private void OnDisable()
+        {
+            EventManager.GameEvents.OnGameOver -= ResetReviveCost;
+            EventManager.GameEvents.OnGameRestart -= ResetReviveCost;
+        }
+
+        public void AddCoins(int amount)
+        {
+            if (amount <= 0) return;
+
+            Coins += amount;
+            Save();
+        }
+
+        public bool TrySpendRevive()
+        {
+            if (!CanAffordRevive) return false;
+
+            Coins -= CurrentReviveCost;
+            CurrentReviveCost *= 2;
+            Save();
+            return true;
+        }
+
+        private void ResetReviveCost()
+        {
+            CurrentReviveCost = BaseReviveCost;
+        }
+
+        private void Save()
+        {
+            PlayerPrefs.SetInt(CoinKey, Coins);
+            PlayerPrefs.Save();
+        }
+    }
+}
--- Assets/Project/Scripts/UI/RevivePanel.cs
+++ Assets/Project/Scripts/UI/RevivePanel.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -9,19 +10,16 @@ namespace VertigoSpin.Project.Scripts.UI
     {
         [field: Header("Buttons")]
         [field: SerializeField] public Button CoinReviveButton{ get; private set; }
-        [field: SerializeField] public Button AdReviveButton{ get; private set; }
         [field: SerializeField] public Button GiveUpButton{ get; private set; }
 
         [Header("Text")]
         [SerializeField] private TextMeshProUGUI reviveCostText;
+        [SerializeField] private TextMeshProUGUI playerCoinsText;
 
         private void OnValidate()
         {
             if (!CoinReviveButton)
                 CoinReviveButton = transform.Find("ui_button_revive_coin")?.GetComponent<Button>();
-
-            if (!AdReviveButton)
-                AdReviveButton = transform.Find("ui_button_revive_ad")?.GetComponent<Button>();
 
             if (!GiveUpButton)
                 GiveUpButton = transform.Find("ui_button_revive_giveup")?.GetComponent<Button>();
@@ -32,11 +30,10 @@ namespace VertigoSpin.Project.Scripts.UI
             if (CoinReviveButton)
                 CoinReviveButton.onClick.AddListener(HandleCoinRevive);
 
-            if (AdReviveButton)
-                AdReviveButton.onClick.AddListener(HandleAdRevive);
-
             if (GiveUpButton)
                 GiveUpButton.onClick.AddListener(HandleGiveUp);
+
+            UpdateUI();
         }
 
         private void OnDisable()
@@ -44,23 +41,64 @@ namespace VertigoSpin.Project.Scripts.UI
             if (CoinReviveButton)
                 CoinReviveButton.onClick.RemoveListener(HandleCoinRevive);
 
-            if (AdReviveButton)
-                AdReviveButton.onClick.RemoveListener(HandleAdRevive);
-
             if (GiveUpButton)
                 GiveUpButton.onClick.RemoveListener(HandleGiveUp);
         }
 
-        private static void HandleCoinRevive()
+        private void UpdateUI()
         {
-            EventManager.GameEvents.FireRevive();
-            UIManager.Instance.HidePanel(PanelType.Revive);
+            int cost = CoinManager.Instance.CurrentReviveCost;
+            bool canAfford = CoinManager.Instance.CanAffordRevive;
+
+            if (CoinReviveButton)
+                CoinReviveButton.interactable = canAfford;
+
+            if (GiveUpButton)
+                GiveUpButton.interactable = true;
+
+            if (reviveCostText)
+                reviveCostText.text = $"Revive {cost}";
+
+            if (playerCoinsText)
+                playerCoinsText.text = CoinManager.Instance.Coins.ToString();
         }
 
-        private static void HandleAdRevive()
+        private const float DialDownDuration = 0.6f;
+        private const float PanelCloseDelay = 0.3f;
+
+        private void HandleCoinRevive()
         {
-            EventManager.GameEvents.FireRevive();
-            UIManager.Instance.HidePanel(PanelType.Revive);
+            int coinsBefore = CoinManager.Instance.Coins;
+            if (!CoinManager.Instance.TrySpendRevive()) return;
+
+            int coinsAfter = CoinManager.Instance.Coins;
+
+            if (CoinReviveButton) CoinReviveButton.interactable = false;
+            if (GiveUpButton) GiveUpButton.interactable = false;
+
+            if (playerCoinsText)
+            {
+                DOTween.To(
+                        () => coinsBefore,
+                        value => playerCoinsText.text = value.ToString(),
+                        coinsAfter,
+                        DialDownDuration)
+                    .SetEase(Ease.OutQuad)
+                    .OnComplete(() =>
+                    {
+                        UIManager.TextAnimation(playerCoinsText);
+                        DOVirtual.DelayedCall(PanelCloseDelay, () =>
+                        {
+                            EventManager.GameEvents.FireRevive();
+                            UIManager.Instance.HidePanel(PanelType.Revive);
+                        });
+                    });
+            }
+            else
+            {
+                EventManager.GameEvents.FireRevive();
+                UIManager.Instance.HidePanel(PanelType.Revive);
+            }
         }
 
         private static void HandleGiveUp()
